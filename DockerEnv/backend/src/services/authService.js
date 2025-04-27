db42bf78a0d995c2fd0bee4fe2abce45d3b04868
novinovinovinovi <thanhdungng04@gmail.com>
2025-04-27 20:38:11 +0700

    Complete Auth API

--- DockerEnv/backend/src/services/authService.js
+++ DockerEnv/backend/src/services/authService.js
@@ -1,7 +1,9 @@
 
 import bcryptjs from 'bcryptjs'
 import { StatusCodes } from 'http-status-codes'
+import { env } from '~/config/environment'
 import { userModel } from '~/models/userModel'
+import { JwtProvider } from '~/providers/JwtProvider'
 import ApiError from '~/utils/ApiError'
 
 const login = async (reqBody) => {
@@ -14,9 +16,26 @@ const login = async (reqBody) => {
     if (!existUser) throw new ApiError(StatusCodes.UNAUTHORIZED, 'Tài khoản không tồn tại!')
 
     const isMatchPassword = bcryptjs.compareSync(password, existUser.password)
-    if (!isMatchPassword) throw new ApiError(StatusCodes.UNAUTHORIZED, 'Mật khẩu không chính xác!')
+    if (!isMatchPassword) throw new ApiError(StatusCodes.NOT_ACCEPTABLE, 'Mật khẩu không chính xác!')
 
-    return existUser
+    const userInfo = {
+      _id: existUser._id,
+      email: existUser.email
+    }
+
+    const accessToken = await JwtProvider.generateToken(
+      userInfo,
+      env.ACCESS_TOKEN_SECRET_SIGNATURE,
+      env.ACCESS_TOKEN_LIFE
+    )
+
+    const refreshToken = await JwtProvider.generateToken(
+      userInfo,
+      env.REFRESH_TOKEN_SECRET_SIGNATURE,
+      env.REFRESH_TOKEN_LIFE
+    )
+
+    return { existUser, accessToken, refreshToken }
   }
   catch (error) {
     throw error
@@ -44,7 +63,33 @@ const register = async (reqBody) => {
   }
 }
 
+const refreshToken = async (clientRefreshToken) => {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const refreshTokenDecoded = await JwtProvider.verify(clientRefreshToken, env.REFRESH_TOKEN_SECRET_SIGNATURE)
+    if (!refreshTokenDecoded) throw new ApiError(StatusCodes.UNAUTHORIZED, 'Refresh token không hợp lệ!')
+
+
+    const userInfo = {
+      _id: refreshTokenDecoded._id,
+      email: refreshTokenDecoded.email
+    }
+
+    const accessToken = await JwtProvider.generateToken(
+      userInfo,
+      env.ACCESS_TOKEN_SECRET_SIGNATURE,
+      env.ACCESS_TOKEN_LIFE
+    )
+
+    return { accessToken }
+  }
+  catch (error) {
+    throw error
+  }
+}
+
 export const authService = {
   register,
-  login
+  login,
+  refreshToken
 }
